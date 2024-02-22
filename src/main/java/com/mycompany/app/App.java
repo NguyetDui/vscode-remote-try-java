9a6196fd45332b33417496c2a99f24f0be13fc81
NguyetDui <160729524+NguyetDui@users.noreply.github.com>
2024-02-22 10:47:52 +0000

    slide6

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,29 @@
  *---------------------------------------------------------------------------------------*/
 
 package com.mycompany.app;
+class Number {
+    public int i;
+
+    // Phương thức để gán giá trị cho thuộc tính i của một đối tượng Number
+    public void setI(int value) {
+        this.i = value;
+    }
+}
 
 public class App {
     public static void main(String[] args) {
+        //assignment 1
         System.out.println("Hello Remote World!");
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        
+        // Gọi phương thức setI để gán giá trị từ n2.i cho n1.i
+        n1.setI(n2.i);
+        
+        n2.i = 10;
+
+        System.out.println("n1.i = " + n1.i); // Kết quả: n1.i = 10
     }
 }
